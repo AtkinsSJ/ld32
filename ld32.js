396ed7ddf27243d27c6f6c8257bd4a7cc1b2d4b6
Sam Atkins <sam@samatkins.co.uk>
2015-04-12 17:29:27 +0100

    Attempting my own framework rather than phaser. It's borked though

--- ld32.js
+++ ld32.js
@@ -2,18 +2,37 @@
  * Sam Atkins' LD32 entry
  */
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-	preload: preload, create: create, update: update
-});
+;function Game() {
+	var canvas = document.getElementById("canvas");
 
-function preload() {
+	this.context2d = canvas.getContext("2d");
+	this.width = canvas.width;
+	this.height = canvas.height;
 
+	// Keyboard handling
+	this.keysPressed = {};
+	this.onkeydown = function(e) {
+		e = e || window.event;
+		this.keysPressed[e.keyCode] = true;
+	};
+	this.onkeyup = function(e) {
+		e = e || window.event;
+		delete this.keysPressed[e.keyCode];
+	};
+	window.onkeydown = this.onkeydown;
+	window.onkeyup = this.onkeyup;
+
+	// Run!
+	this.start = function() {
+		window.requestAnimationFrame(this.update);
+	};
+	this.update = function() {
+
+		this.context2d.clearRect(0,0, width,height);
+
+		window.requestAnimationFrame(this.update);
+	};
 }
 
-function create() {
-
-}
-
-function update() {
-	
-}
+var game = new Game();
+game.start();
