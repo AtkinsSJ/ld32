33a306dc676aa21f14604592fbf59f125dc87577
Sam Atkins <sam@samatkins.co.uk>
2015-04-12 17:37:26 +0100

    Things work now! Next I need to decide on some structure, beyond an empty game loop

--- ld32.js
+++ ld32.js
@@ -2,37 +2,35 @@
  * Sam Atkins' LD32 entry
  */
 
-;function Game() {
-	var canvas = document.getElementById("canvas");
+;
 
-	this.context2d = canvas.getContext("2d");
-	this.width = canvas.width;
-	this.height = canvas.height;
+var canvas = document.getElementById("canvas");
 
-	// Keyboard handling
-	this.keysPressed = {};
-	this.onkeydown = function(e) {
-		e = e || window.event;
-		this.keysPressed[e.keyCode] = true;
-	};
-	this.onkeyup = function(e) {
-		e = e || window.event;
-		delete this.keysPressed[e.keyCode];
-	};
-	window.onkeydown = this.onkeydown;
-	window.onkeyup = this.onkeyup;
+var Game = {
+	context2d: canvas.getContext("2d"),
+	width: canvas.width,
+	height: canvas.height,
+	keysPressed: {},
+};
 
-	// Run!
-	this.start = function() {
-		window.requestAnimationFrame(this.update);
-	};
-	this.update = function() {
+// Keyboard handling
+window.onkeydown = function(e) {
+	e = e || window.event;
+	Game.keysPressed[e.keyCode] = true;
+};
+window.onkeyup = function(e) {
+	e = e || window.event;
+	delete Game.keysPressed[e.keyCode];
+};
 
-		this.context2d.clearRect(0,0, width,height);
+// Run!
+function main() {
+	window.requestAnimationFrame(main);
 
-		window.requestAnimationFrame(this.update);
-	};
+	Game.context2d.clearRect(0,0, Game.width,Game.height);
+
+
 }
 
-var game = new Game();
-game.start();
+
+main();
