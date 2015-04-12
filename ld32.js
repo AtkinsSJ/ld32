fba9b7aff3b8746a691e59746d2d7ce521a74a76
Sam Atkins <sam@samatkins.co.uk>
2015-04-12 17:58:13 +0100

    Basic menu! :D

--- ld32.js
+++ ld32.js
@@ -11,17 +11,44 @@ var Game = {
 	width: canvas.width,
 	height: canvas.height,
 	keysPressed: {},
+	scene: new MenuScene(),
 };
 
 // Keyboard handling
 window.onkeydown = function(e) {
 	e = e || window.event;
 	Game.keysPressed[e.keyCode] = true;
+	console.log("Pressed key", Game.keysPressed);
+	e.preventDefault();
 };
 window.onkeyup = function(e) {
 	e = e || window.event;
 	delete Game.keysPressed[e.keyCode];
 };
+
+function MenuScene() {
+	this.update = function() {
+
+		// Go to play scene
+		if (Game.keysPressed[32]) {
+			Game.scene = new PlayScene();
+		}
+
+		// Render text
+		Game.context2d.font = "20px sans-serif";
+		Game.context2d.fillStyle = "white";
+		Game.context2d.textAlign = "center";
+		Game.context2d.textBaseline = "middle";
+		Game.context2d.fillText("LD32 game!", Game.width/2, 20);
+		Game.context2d.fillText("Press Space to play!", Game.width/2, Game.height/3);
+	};
+}
+
+function PlayScene() {
+	this.update = function() {
+
+	};
+}
 
 // Run!
 function main() {
@@ -29,7 +56,7 @@ function main() {
 
 	Game.context2d.clearRect(0,0, Game.width,Game.height);
 
-
+	Game.scene.update();
 }
 
 
